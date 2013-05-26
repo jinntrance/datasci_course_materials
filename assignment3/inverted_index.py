4fe5000ea703acb730cc304c3ae4292a4370fc17
Joseph <jinntrance@gmail.com>
2013-05-27 00:34:39 +0800

    problem 1

---
+++ assignment3/inverted_index.py
@@ -0,0 +1,31 @@
+import MapReduce
+import sys
+
+"""
+assignment 3, problem 1
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document text
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    mr.emit((key, list(set(list_of_values))))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
