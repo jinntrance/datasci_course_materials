da4deecc9854e8a3bf0294d7603f72ddd6f1de41
Joseph <jinntrance@gmail.com>
2013-05-28 00:18:19 +0800

    assignment 3 finished

--- assignment3/multiply.py
+++ assignment3/multiply.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import MapReduce
 import sys
 
@@ -10,21 +11,47 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
-def mapper(record):
-    # key: id
-    # value: sequence
-    key = record[0]
-    value = record[1]
-    mr.emit_intermediate(value[:-10],1)
+Ldim = 10 #TODO hard code is evil
+Ndim = 10
 
-def reducer(key, list_of_values):
-    # key: id
+
+def mapper(record):
+    # key: (row,col)
+    # value: record
+    matrix=record[0]
+    row=record[1]
+    column=record[2]
+    if "a" == matrix:
+        for k in range(Ndim):
+            mr.emit_intermediate((row,k), record)
+    if "b" == matrix:
+        for i in range(Ldim):
+            mr.emit_intermediate((i,column),record)
+
+def reducer(key, elements):
+    # key: (row,col)
     # value: list of sequence
     # for e in set(list_of_values):
-    mr.emit(key)
+    rowA= []
+    columnB= []
+    total=0
+    for e in sorted(elements):
+        if "a" == e[0]:
+            rowA.append(e)
+        if "b" == e[0]:
+            columnB.append(e)
+    for a in sorted(rowA,key=itemgetter(1)):
+        print a
+        for b in sorted(columnB,key=itemgetter(1,2)):
+            if a[2]==b[1]:
+                total+=a[3]*b[3]
+    if 0 !=total:
+        r,c=key
+        mr.emit((r,c,total))
 
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':
   inputdata = open(sys.argv[1])
+  # mr.execute(inputdata, dimM,reducer)
   mr.execute(inputdata, mapper, reducer)
